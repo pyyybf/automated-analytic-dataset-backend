22e24166f676087547cb939b9cbe3c72f2eba0a2
pyyybf <pyyybf@gmail.com>
2024-02-11 08:17:51 -0800

    fix: only set up once before all test cases

--- template/autograder/tests/test_simple.py
+++ template/autograder/tests/test_simple.py
@@ -6,7 +6,8 @@ from utils import fetch_dataset, preprocess_nb, execute_nb, get_cell_output, com
 
 
 class TestDemo(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         # Get the student's id
         with open("/autograder/submission_metadata.json", "r") as fp:
             data = json.load(fp)
@@ -16,9 +17,9 @@ class TestDemo(unittest.TestCase):
         fetch_dataset("#assignment_id", usc_id, output_path="dataset.csv")
 
         # Preprocess the notebooks and get the index mapping of question cells
-        self.cell_mapping_stu = preprocess_nb("demo.ipynb")
-        self.cell_mapping_sol = preprocess_nb("solution.ipynb")
+        cls.cell_mapping_stu = preprocess_nb("demo.ipynb")
+        cls.cell_mapping_sol = preprocess_nb("solution.ipynb")
 
         # Read and execute the two notebooks
-        self.nb_stu = execute_nb("demo.ipynb")
-        self.nb_sol = execute_nb("solution.ipynb")
+        cls.nb_stu = execute_nb("demo.ipynb")
+        cls.nb_sol = execute_nb("solution.ipynb")
